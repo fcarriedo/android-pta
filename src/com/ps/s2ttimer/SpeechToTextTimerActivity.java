197f841726250a4735b7c39eadebcf899d18e3de
Francisco J. Carriedo <fcarriedo@gmail.com>
2010-03-02 10:38:30 -0800

    Added basic chronometer functionality

--- src/com/ps/s2ttimer/SpeechToTextTimerActivity.java
+++ src/com/ps/s2ttimer/SpeechToTextTimerActivity.java
@@ -2,12 +2,66 @@ package com.ps.s2ttimer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Chronometer.OnChronometerTickListener;
 
 public class SpeechToTextTimerActivity extends Activity {
     /** Called when the activity is first created. */
+	
+	private Chronometer chrono;
+	
+	private Button startButton;
+	private Button stopButton;
+	private Button resetButton;
+	
+	private TextView timerMsg;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        chrono = (Chronometer)findViewById(R.id.chronometer);
+        chrono.setOnChronometerTickListener(new OnChronometerTickListener() {
+        	@Override
+        	public void onChronometerTick(Chronometer chron) {
+        		timerMsg.setText("Elapsed time : " + (SystemClock.elapsedRealtime()-chrono.getBase()));
+        	}
+        });
+        
+        timerMsg = (TextView)findViewById(R.id.time_msg);
+        
+        startButton = (Button)findViewById(R.id.start);
+        startButton.setOnClickListener(new Button.OnClickListener() {
+        	@Override
+        	public void onClick(View view) {
+        		chrono.start();
+        	}
+        });
+        
+        stopButton = (Button)findViewById(R.id.stop);
+        stopButton.setOnClickListener(new Button.OnClickListener() {
+        	@Override
+        	public void onClick(View view) {
+        		chrono.stop();
+        	}
+        });
+        
+        resetButton = (Button)findViewById(R.id.reset);
+        resetButton.setOnClickListener(new Button.OnClickListener() {
+        	@Override
+        	public void onClick(View view) {
+        		chrono.setBase(SystemClock.elapsedRealtime());
+        	}
+        });
+    }
+    
+    @Override
+    public void onPause() {
+    	// Save application state or clear state.   	
     }
 }
